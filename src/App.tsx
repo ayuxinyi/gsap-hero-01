3a7f3569569070d0db895f2049731a624c80cb36
陈无敌 <yxy_js_code@163.com>
2025-12-24 16:12:21 +0800

    feat: 为 Hero 组件添加滚动视差效果并调整导出方式

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import { Navbar } from "./components/hero/nav-bar";
-import Hero from "./components/hero/hero";
+import { Hero } from "./components/hero/hero";
 
 // 注册滚动触发器插件以及文本分割插件，使它们在全局范围内可用，我们只需要在应用程序的入口文件中注册一次即可。
 gsap.registerPlugin(ScrollTrigger, SplitText);
